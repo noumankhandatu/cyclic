d80dd4954af81d6983f0c840734a562aa4d666de
noumankhandatu <noumankhan619.915@gmail.com>
2024-02-12 02:21:19 +0500

    test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Import required modules
+const dotenv = require("dotenv");
 const express = require("express");
 
 // Create an Express application
@@ -6,9 +7,11 @@ const app = express();
 
 // Define the port to listen on
 const port = process.env.PORT || 5000;
+
 // Define a route handler for the root path
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+  res.send("Hello, World!" + process.env.NOUMAN);
 });
 
 // Start the server, listening on the specified port
