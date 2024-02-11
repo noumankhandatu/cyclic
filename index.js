555cabae5d5ff3f4dea25d67427aa4048e277480
noumankhandatu <noumankhan619.915@gmail.com>
2024-02-12 02:38:39 +0500

    tes

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const app = express();
 
 // Define the port to listen on
 const port = process.env.PORT || 5000;
+console.log(process.env.NOUMAN, "process.env.NOUMAN");
+console.log(process.env.CYCLIC_APP_NOUMAN, "process.env.NOUMAN");
 
 // Define a route handler for the root path
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, World!  process.env.NOUMAN");
+  res.send(
+    `Hello, World! ${process.env.CYCLIC_APP_NOUMAN}  process.env.NOUMAN ${process.env.NOUMAN}`
+  );
+
   res.send("Hello, World!" + process.env.NOUMAN);
   res.send("Hello, World!" + process.env.CYCLIC_APP_NOUMAN);
 });
